208864843b247f51f1d1392fd02bb597ad4f1c6f
Laurazzy <lauramacuer@gmail.com>
2026-08-25 10:48:56 -0400

    INIT

---
+++ Assets/Scripts/hola.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+public class hola : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Console.WriteLine("Hola soy una prueba");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
